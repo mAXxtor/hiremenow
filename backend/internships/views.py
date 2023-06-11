82a33405da3de73ac2a4e65b5d3536c1542dd3b4
Max Verbitskiy <maxxtor21@gmail.com>
2023-06-11 15:45:58 +0400

    fix url

--- backend/internships/views.py
+++ backend/internships/views.py
@@ -5,6 +5,30 @@ class IndexView(TemplateView):
     template_name = 'internships/index.html'
 
 
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+from . import utils
+from .forms import CommentForm, PostForm
+from .models import Follow, Group, Post, User
+
+
+def index(request):
+    posts = Post.objects.select_related('author', 'group').all()
+    context = {'page_obj': utils.paginator(request, posts)}
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.select_related('author').all()
+    context = {
+        'group': group,
+        'page_obj': utils.paginator(request, posts)
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
 class DesignView(TemplateView):
     template_name = 'internships/index.html'
 
