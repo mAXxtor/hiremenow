caae94bb92f42f33dde0a9408019fc6429995904
Max Verbitskiy <maxxtor21@gmail.com>
2023-06-11 16:36:00 +0400

    add django reload

--- backend/hiremenow/urls.py
+++ backend/hiremenow/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hrsite.urls', namespace='hrsite')),
     # path('internships/', include('internships.urls', namespace='internships')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 if settings.DEBUG:
