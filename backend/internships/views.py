b5cf6b59cc052d3da9aac82c3633d41cf3365e3e
Max Verbitskiy <maxxtor21@gmail.com>
2023-06-12 17:09:34 +0400

    add view for internships index

--- backend/internships/views.py
+++ backend/internships/views.py
@@ -10,32 +10,8 @@ class IndexView(TemplateView):
 
 def index(request):
     internships = Internship.objects.prefetch_related('fields').all()
-    # internships = Internship.objects.all()
     return render(request, 'internships/index.html',
                   context={'internships': internships})
-
-# from django.contrib.auth.decorators import login_required
-# from django.shortcuts import get_object_or_404, redirect, render
-
-# from . import utils
-# from .forms import CommentForm, PostForm
-# from .models import Follow, Group, Post, User
-
-
-# def index(request):
-#     posts = Post.objects.select_related('author', 'group').all()
-#     context = {'page_obj': utils.paginator(request, posts)}
-#     return render(request, 'posts/index.html', context)
-
-
-# def group_posts(request, slug):
-#     group = get_object_or_404(Group, slug=slug)
-#     posts = group.posts.select_related('author').all()
-#     context = {
-#         'group': group,
-#         'page_obj': utils.paginator(request, posts)
-#     }
-#     return render(request, 'posts/group_list.html', context)
 
 
 class DesignView(TemplateView):
